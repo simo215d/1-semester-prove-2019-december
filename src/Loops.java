f44d53c9e7748351ffd85dba93f2b1b2f0934a9b
simo215d <simo215d@edu.easj.dk>
2019-12-20 09:35:34 +0100

    Opgave 4 færdig.

--- src/Loops.java
+++ src/Loops.java
@@ -1,3 +1,7 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
 /**
  * Developed by András Ács (acsandras@gmail.com)
  * Zealand / www.zealand.dk
@@ -26,22 +30,30 @@ public class Loops {
      * Viser en liste med brugere i konsollen
      */
     public void printBrugereArr() {
-
-        // TODO Din kode her
+        for (int i = 0; i < getBrugere().length; i++) {
+            System.out.println("navn: "+getBrugere()[i]);
+        }
     }
 
     /**
      * Viser en liste med brugere i konsollen i omvendt rækkefølge
      */
     public void printBrugereReverseArr() {
-
-        // TODO Din kode her
-
+        for (int i = getBrugere().length-1; i >= 0; i--) {
+            System.out.println("navn: "+getBrugere()[i]);
+        }
     }
 
     public void waitingFor0() {
-
-        // TODO Din kode her
- 
+        Scanner input = new Scanner(System.in);
+        while (true){
+            System.out.print("Venter på 0. Skriv et tal... ");
+            int n = input.nextInt();
+            if (n!=0) {
+                continue;
+            }
+            System.out.println("Du indtastede 0, og loopet er færdig.");
+            break;
+        }
     }
 }
